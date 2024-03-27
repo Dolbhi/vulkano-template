8c70abcb3176106f75e10c36d9fb0de93bdd21b4
Dolbhi <110004436+Dolbhi@users.noreply.github.com>
2024-03-27 18:48:42 +1100

    moved damp delta time calc to proper place

--- src/game_objects/utility.rs
+++ src/game_objects/utility.rs
@@ -19,11 +19,12 @@ impl VectorDamp {
     pub fn smooth_follow(&mut self, current: Vector3<f32>, target: Vector3<f32>) -> Vector3<f32> {
         let difference = current - target;
 
-        self.current_velocity -= self.strength
-            * (2. * self.current_velocity + self.strength * difference)
-            * self.last_time.elapsed().as_secs_f32();
+        self.current_velocity -=
+            self.strength * (2. * self.current_velocity + self.strength * difference);
+
+        let elapsed_time = self.last_time.elapsed().as_secs_f32();
         self.last_time = Instant::now();
 
-        current + self.current_velocity
+        current + self.current_velocity * elapsed_time
     }
 }
