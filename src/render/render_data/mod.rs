39bb9a6cee4dfd68d0628521898c15ef59d35d2d
Dolbhi <110004436+Dolbhi@users.noreply.github.com>
2024-07-20 00:22:16 +0900

    added documentation explaining render object hierarchy

--- src/render/render_data/mod.rs
+++ src/render/render_data/mod.rs
@@ -1,3 +1,10 @@
+/// Rendering Heirachy:
+///
+/// System < Shader < Material < Object
+///
+/// Limitations:
+/// 1. Shader and Material must be efficiently searched for
+/// 2. Some shaders require specific material sets
 pub mod material;
 pub mod mesh;
 pub mod render_object;
