6a509b7ba17289665e933356f309b4d2fcc42e92
Dolbhi <110004436+Dolbhi@users.noreply.github.com>
2026-09-19 18:49:31 +1000

    added model applying helper func

--- src/utilities/math.rs
+++ src/utilities/math.rs
@@ -1,6 +1,6 @@
 use std::ops::Neg;
 
-use cgmath::{num_traits::Num, Matrix3, Vector3};
+use cgmath::{num_traits::Num, BaseFloat, Matrix3, Matrix4, One, Vector3};
 
 /// Create an antisymmetric skew matrix with the given vector components
 pub fn skew<S: Num + Neg<Output = S> + Copy>(v: Vector3<S>) -> Matrix3<S> {
@@ -9,4 +9,8 @@ pub fn skew<S: Num + Neg<Output = S> + Copy>(v: Vector3<S>) -> Matrix3<S> {
         y: (-v.z, S::zero(), v.x).into(),
         z: (v.y, -v.x, S::zero()).into(),
     }
+}
+
+pub fn apply_model<S: BaseFloat + One>(m: Matrix4<S>, v: Vector3<S>) -> Vector3<S> {
+    (m * v.extend(S::one())).truncate()
 }
